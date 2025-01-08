4618bd821c7ab12f3cbfed53ba2219ef9d1cf075
frogeR <lesnerblazej2@gmail.com>
2025-01-08 18:36:35 +0000

    Version good enougth

--- scripts/script1.js
+++ scripts/script1.js
@@ -6,11 +6,17 @@ const PlayerBarRestartButton = document.getElementById("PlayerBarRestartButton")
 const PlayerBarPlayButtonImg = document.getElementById("PlayerBarPlayButtonImg")
 const PlayerBarPlayButton = document.getElementById("PlayerBarPlayButton")
 
+const CurrentSongTitleElement = document.getElementById("CurrentSongTitleElement")
+
 const PlayerBarPlayNextButton = document.getElementById("PlayerBarPlayNextButton")
+const CurrentSongAuthorElement = document.getElementById("CurrentSongAuthorElement")
+const CurrentSongDateElement = document.getElementById("CurrentSongDateElement")
 
+const PlayButton1 = document.getElementById("PlayButton1")
+const PlayButton2 = document.getElementById("PlayButton2")
 
+const Progressbar = document.getElementById("Progressbar")
 
-const current_audio = new Audio("audio\\Walk Through the Park.mp3");
 
 //vars
 let playing = false
@@ -18,24 +24,52 @@ let playing = false
 let CurrentSongTitle = "Title"
 let CurrentSontAuthor = "Author"
 let CurrentDate = "00/00/0000"
+let CurrentAudio = new Audio("");
 
-//func
+// Will execute update_progress_bar every 1 seconds 
+var intervalID = window.setInterval(update_progress_bar, 1000);
+
+
+function update_progress_bar() {
+    if (playing) {
+        Progressbar.value = CurrentAudio.currentTime
+        Progressbar.max = CurrentAudio.duration
+    }
+}
+
+function update_current_audio() {
+    CurrentSongTitleElement.textContent = CurrentSongTitle
+    CurrentSongAuthorElement.textContent = CurrentSontAuthor
+    CurrentSongDateElement.textContent = CurrentDate
+}
+
+
+function set_current_audio(Audio_File, Title, Author, Date) {
+    playing = true
+    toggle_play()
+
+    console.log("playing song")
+    CurrentAudio = new Audio(Audio_File);
+    CurrentSongTitle = Title
+    CurrentSontAuthor = Author
+    CurrentDate = Date
+    update_current_audio()
+}
+
 function autoload() {
     
 }
 
 function toggle_play() {
-    console.log("test");
-    
     if (playing == true) {
         playing = false
         PlayerBarPlayButtonImg.src = "assets\\play_arrow.svg"
-        current_audio.pause()
+        CurrentAudio.pause()
     }
     else {
         playing = true
         PlayerBarPlayButtonImg.src = "assets\\pause.svg"
-        current_audio.play()
+        CurrentAudio.play()
     }
 }
 
@@ -55,4 +89,14 @@ PlayerBarPlayNextButton.onclick = function() {
 
 PlayerBarRestartButton.onclick = function() {
     console.log("restart");
+}
+
+PlayButton1.onclick = function() {
+    console.log("play song")
+    set_current_audio("audio\\Walk Through the Park.mp3", "Walk Through the Park", "Track Tribe", "(21/4/2024)")
+}
+
+PlayButton2.onclick = function() {
+    console.log("play song")
+    set_current_audio("audio\\Alan Walker - Faded.mp3", "Faded", "Alan Walker", "(4/12/2015)")
 }
